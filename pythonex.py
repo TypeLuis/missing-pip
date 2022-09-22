5a5287d0fab42eefab594be411af973b9814b98f
Luis Lopez <typeluis@gmail.com>
2022-09-21 20:25:01 -0400

    install packages if missing

--- pythonex.py
+++ pythonex.py
@@ -10,3 +10,10 @@ missing = required - installed
 
 print(missing)
 
+if missing:
+    # implement pip as a subprocess:
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', *missing])
+
+from colorama import Fore
+
+print(f'{Fore.GREEN} Hi with green! {Fore.RESET}')
